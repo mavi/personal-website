69c88b6b3b17a8de6918af0ba4bf3b4e76991d69
Mavi <erewnoz@gmail.com>
2026-01-13 22:46:35 +0300

    s

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
     return (
         <main className="flex min-h-screen flex-col items-center justify-center p-4 sm:p-8 md:p-24 overflow-hidden relative bg-black">
             <SnowEffect />
-            <div className="z-10 w-full flex justify-center">
+            <div className="relative z-10 w-full flex justify-center">
                 <ProfileCard data={profileData as ProfileData} />
             </div>
         </main>
