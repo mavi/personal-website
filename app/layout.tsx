7fcac60cb13366584c8ae439304a1a2c19997092
Mavi <erewnoz@gmail.com>
2026-01-14 01:46:02 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "Personal Website",
-  description: "My personal portfolio",
+  title: "eren.live",
+  description: "welcome.",
 };
 
 export default function RootLayout({
